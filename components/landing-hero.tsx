0d886f04c76407aee583e0eb7e35fe66597e4b57
anish1802 <anishraj17226016@gmail.com>
2024-08-15 11:56:15 +0530

    changes-landing

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -4,21 +4,13 @@ import { useAuth } from "@clerk/nextjs";
 import TypewriterComponent from "typewriter-effect";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Vortex } from "./ui/vortex";
+
 export const LandingHero = () => {
   const { isSignedIn } = useAuth();
   return (
-
-    <>
-    {/* <Vortex
-        backgroundColor='dark-gray'
-        particleCount={50}
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-      > */}
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
-        
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
@@ -32,7 +24,7 @@ export const LandingHero = () => {
               autoStart: true,
               loop: true,
             }}
-            />
+          />
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
@@ -43,7 +35,7 @@ export const LandingHero = () => {
           <Button
             variant="premium"
             className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
-            >
+          >
             Start Generating for Free
           </Button>
         </Link>
@@ -52,7 +44,5 @@ export const LandingHero = () => {
         No credit card required.
       </div>
     </div>
-              {/* </Vortex> */}
-            </>
   );
 };
